3e5f9405ab3e881ff651c75276e292aeb2d249aa
Minkyu Park <minkyu@oleander.dev>
2025-11-14 07:52:45 -0800

    Change sha256 for new bundle.
    Signed-off-by: Minkyu Park <minkyu@oleander.dev>

--- Formula/oleander-cli.rb
+++ Formula/oleander-cli.rb
@@ -2,7 +2,7 @@ class OleanderCli < Formula
   desc "Oleander CLI"
   homepage "https://oleander.dev"
   url "https://oleander-cli-releases.s3.amazonaws.com/oleander-cli-oleander-cli-v0.1.0.tar.gz"
-  sha256 "6bf12025fbfa03c72b0cd5775e48995a1066babf668fc1b48fb64f50cd7da40b"
+  sha256 "e669d7d0e4f98ed37ca23a92bdbdfadb8ce506bf7f755b561557caebd6be0e1b"
   license "LicenseRef-OleanderHQ-Proprietary"
 
   depends_on "node"
