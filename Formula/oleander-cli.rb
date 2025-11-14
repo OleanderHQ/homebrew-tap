01bee651ff5c3c50f7bb880af4f8a0adcd057458
Minkyu Park <minkyu@oleander.dev>
2025-11-13 16:20:33 -0800

    Add oleander-cli formula
    Signed-off-by: Minkyu Park <minkyu@oleander.dev>

---
+++ Formula/oleander-cli.rb
@@ -0,0 +1,17 @@
+class OleanderCli < Formula
+  desc "Oleander CLI"
+  homepage "https://oleander.dev"
+  url "https://oleander-cli-releases.s3.amazonaws.com/oleander-cli-oleander-cli-v0.1.0.tar.gz"
+  sha256 "27da32b5737e9be5e166065c9d4b9b229e8ebeeca766d097aad1a01fce5a582a"
+  license "LicenseRef-OleanderHQ-Proprietary"
+
+  depends_on "node"
+
+  def install
+    bin.install "oleander"
+  end
+
+  test do
+    system "#{bin}/oleander", "--help"
+  end
+end
