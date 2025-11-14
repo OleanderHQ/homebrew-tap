16612bf081acbc25176c1a42271447d3c040a400
Minkyu Park <minkyu@oleander.dev>
2025-11-14 11:05:16 -0800

    Put correct sha256 for the replaced artifact.
    Signed-off-by: Minkyu Park <minkyu@oleander.dev>

--- Formula/oleander-cli.rb
+++ Formula/oleander-cli.rb
@@ -2,7 +2,7 @@ class OleanderCli < Formula
   desc "Oleander CLI"
   homepage "https://oleander.dev"
   url "https://oleander-cli-releases.s3.amazonaws.com/oleander-cli-oleander-cli-v0.1.0.tar.gz"
-  sha256 "e669d7d0e4f98ed37ca23a92bdbdfadb8ce506bf7f755b561557caebd6be0e1b"
+  sha256 "a28033b7c47c0d1eda65b2958e5f806618ba7fc23d5ecff620968c2647798cad"
   license "LicenseRef-OleanderHQ-Proprietary"
 
   depends_on "node"
