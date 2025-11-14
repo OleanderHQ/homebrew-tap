d5bf8a0a8ebfa324b3ebf2290d7ea7c689b5cd45
Minkyu Park <minkyu@oleander.dev>
2025-11-14 07:36:10 -0800

    Put correct shasum
    Signed-off-by: Minkyu Park <minkyu@oleander.dev>

--- Formula/oleander-cli.rb
+++ Formula/oleander-cli.rb
@@ -2,7 +2,7 @@ class OleanderCli < Formula
   desc "Oleander CLI"
   homepage "https://oleander.dev"
   url "https://oleander-cli-releases.s3.amazonaws.com/oleander-cli-oleander-cli-v0.1.0.tar.gz"
-  sha256 "27da32b5737e9be5e166065c9d4b9b229e8ebeeca766d097aad1a01fce5a582a"
+  sha256 "6bf12025fbfa03c72b0cd5775e48995a1066babf668fc1b48fb64f50cd7da40b"
   license "LicenseRef-OleanderHQ-Proprietary"
 
   depends_on "node"
