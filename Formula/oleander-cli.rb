29b1dbc91507c3c17ec341c0ff1354724768cf7b
Willy Lulciuc <willy@oleander.dev>
2025-12-15 18:20:01 -0500

    Release `0.2.0`

--- Formula/oleander-cli.rb
+++ Formula/oleander-cli.rb
@@ -1,8 +1,8 @@
 class OleanderCli < Formula
   desc "Oleander CLI"
   homepage "https://oleander.dev"
-  url "https://oleander-cli-releases.s3.amazonaws.com/oleander-cli-oleander-cli-v0.1.2.tar.gz"
-  sha256 "cb261cbf59aec2b94b07a1bebc0cc6c36b145219e6812471a44462e762bd0902"
+  url "https://oleander-cli-releases.s3.amazonaws.com/oleander-cli-oleander-cli-v0.2.0.tar.gz"
+  sha256 "e57bc19cb66ae1bbf9568154876dec710cdbd6693ac37b5fa7d832f487c2eade"
   license "LicenseRef-OleanderHQ-Proprietary"
 
   depends_on "node"
