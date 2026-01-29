501535c285be3800987658d285d839c6f95aa76b
Minkyu Park <minkyu@oleander.dev>
2026-01-29 13:55:22 +0900

    Relase 0.5.0
    Signed-off-by: Minkyu Park <minkyu@oleander.dev>

--- Formula/oleander-cli.rb
+++ Formula/oleander-cli.rb
@@ -1,8 +1,8 @@
 class OleanderCli < Formula
   desc "Oleander CLI"
   homepage "https://oleander.dev"
-  url "https://oleander-cli-releases.s3.amazonaws.com/oleander-cli-oleander-cli-v0.4.0.tar.gz"
-  sha256 "28064a071d50329364c452275d193d8fd7199ca61bafa44e88d9bb2a51d589bc"
+  url "https://oleander-cli-releases.s3.amazonaws.com/oleander-cli-oleander-cli-v0.5.0.tar.gz"
+  sha256 "c56daf158c37281faf6f6bc4bfb5dcccb14b696f04b8700190c5a585dc2c94cf"
   license "LicenseRef-OleanderHQ-Proprietary"
 
   depends_on "node"
