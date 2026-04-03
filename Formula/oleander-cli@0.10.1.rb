97cce20b6b799c5ff5281ee2ebdb68678146cf7d
Minkyu Park <minkyu@oleander.dev>
2026-04-03 14:31:06 -0700

    Release 0.10.1
    Signed-off-by: Minkyu Park <minkyu@oleander.dev>

--- Formula/oleander-cli@0.10.1.rb
+++ Formula/oleander-cli@0.10.1.rb
@@ -1,9 +1,12 @@
-class OleanderCli < Formula
+class OleanderCliAT0101 < Formula
   desc "Oleander CLI"
   homepage "https://oleander.dev"
   url "https://oleander-cli-releases.s3.amazonaws.com/oleander-cli-oleander-cli-v0.10.1.tar.gz"
+  version "0.10.1"
   sha256 "9ae239dc244cadbe7cefbd6ca33e64b7e1f128cfb5ab9a61cb2d20ad9b31c517"
   license "LicenseRef-OleanderHQ-Proprietary"
+
+  keg_only :versioned_formula
 
   depends_on "node"
   depends_on "uv"
