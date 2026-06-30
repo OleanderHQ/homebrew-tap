92eaf4578d7bc3c93645c546363a44f1df53a76d
Minkyu Park <minkyu@oleander.dev>
2026-06-30 11:18:01 -0700

    Update oleander-cli to 0.14.0
    Signed-off-by: Minkyu Park <minkyu@oleander.dev>

--- Formula/oleander-cli@0.13.1.rb
+++ Formula/oleander-cli@0.13.1.rb
@@ -1,9 +1,12 @@
-class OleanderCli < Formula
+class OleanderCliAT0131 < Formula
   desc "Oleander CLI"
   homepage "https://oleander.dev"
-  url "https://oleander-cli-releases.s3.amazonaws.com/oleander-cli-oleander-cli-v0.14.0.tar.gz"
-  sha256 "c5a1415ec07eef6048bf41083f66040353b39d3f95583d7afcdf7eea2361b5d1"
+  url "https://oleander-cli-releases.s3.amazonaws.com/oleander-cli-oleander-cli-v0.13.1.tar.gz"
+  version "0.13.1"
+  sha256 "036db7d68f6212a9f13efa3f763b9ad26441f1561046911c113d6b8e54d5551c"
   license "LicenseRef-OleanderHQ-Proprietary"
+
+  keg_only :versioned_formula
 
   depends_on "node"
   depends_on "uv"
--- Formula/oleander-cli@0.14.0.rb
+++ Formula/oleander-cli@0.14.0.rb
@@ -1,9 +1,12 @@
-class OleanderCli < Formula
+class OleanderCliAT0140 < Formula
   desc "Oleander CLI"
   homepage "https://oleander.dev"
   url "https://oleander-cli-releases.s3.amazonaws.com/oleander-cli-oleander-cli-v0.14.0.tar.gz"
+  version "0.14.0"
   sha256 "c5a1415ec07eef6048bf41083f66040353b39d3f95583d7afcdf7eea2361b5d1"
   license "LicenseRef-OleanderHQ-Proprietary"
+
+  keg_only :versioned_formula
 
   depends_on "node"
   depends_on "uv"
